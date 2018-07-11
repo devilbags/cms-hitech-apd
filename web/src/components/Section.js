aea1729c6923d4eb27c1bfdd4d35f6e2eb826e9c
Brendan Sudol <brendan.sudol@gsa.gov>
2018-07-11 11:05:34 -0400

    section scroll track (for sidebar)

--- web/src/components/Section.js
+++ web/src/components/Section.js
@@ -1,10 +1,59 @@
 import PropTypes from 'prop-types';
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import { withRouter } from 'react-router';
+import Waypoint from 'react-waypoint';
+
 import Collapsible from './Collapsible';
 import HelpText from './HelpText';
 import SectionTitle from './SectionTitle';
 import SectionDesc from './SectionDesc';
 import { t } from '../i18n';
+
+const ScrollWatcher = Wrapped =>
+  withRouter(
+    class extends Component {
+      static propTypes = {
+        id: PropTypes.string,
+        location: PropTypes.object.isRequired,
+        history: PropTypes.object.isRequired
+      };
+
+      static defaultProps = {
+        id: null
+      };
+
+      handleEnter = ({ currentPosition, previousPosition }) => {
+        if (currentPosition === 'inside' && previousPosition === 'above') {
+          this.updateUrl();
+        }
+      };
+
+      handleLeave = ({ currentPosition, previousPosition }) => {
+        if (currentPosition === 'above' && previousPosition === 'inside') {
+          this.updateUrl();
+        }
+      };
+
+      updateUrl = () => {
+        const { id, location, history } = this.props;
+        const newHash = `#${id}`;
+
+        if (location.hash === newHash) return;
+        history.replace(newHash);
+      };
+
+      render() {
+        if (!this.props.id) return <Wrapped {...this.props} />;
+
+        return (
+          <div>
+            <Waypoint onEnter={this.handleEnter} onLeave={this.handleLeave} />
+            <Wrapped {...this.props} />
+          </div>
+        );
+      }
+    }
+  );
 
 const Section = ({ children, id, resource }) => {
   const title = t([resource, 'title'], { defaultValue: false });
@@ -81,5 +130,8 @@ Subsection.defaultProps = {
   open: false
 };
 
-export default Section;
-export { Chunk, Section, Subsection };
+const ScrollSection = ScrollWatcher(Section);
+const ScrollSubsection = ScrollWatcher(Subsection);
+
+export default ScrollSection;
+export { Chunk, ScrollSection as Section, ScrollSubsection as Subsection };
